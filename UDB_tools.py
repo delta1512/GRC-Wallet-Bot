066a18911da16f9def626acdb3a044c2a7669c91
Unknown <22047487+delta1512@users.noreply.github.com>
2018-04-04 22:51:09 +1000

    Moved databasing tools to async

--- UDB_tools.py
+++ UDB_tools.py
@@ -1,45 +1,55 @@
-import subprocess as sp
 from user import usr
-import sqlite3
+import grcconf as g
+import aiomysql
+import asyncio
 
-def create_db(dbdir):
-    db = sqlite3.connect(dbdir)
-    c = db.cursor()
-    c.execute('''CREATE TABLE udb (
-    userID text,
-    address text,
-    last_active int,
-    balance float,
-    donations float,
-    lastTX_amt float,
-    lastTX_time int,
-    lastTX_txid text
-    )''')
-    db.commit()
-    db.close()
+async def check_db():
+    try:
+        conn = await aiomysql.connect(host=g.sql_db_host, port=3306, user=g.sql_db_usr,
+                                    password=g.sql_db_pass, db='mysql')
+        c = await conn.cursor()
+        await c.execute('SELECT * FROM grcbot.udb')
+    except:
+        return 1
+    conn.close()
+    return 0
 
-def read_db(dbdir):
-    global UDB
-    db = sqlite3.connect(dbdir)
-    c = db.cursor()
-    c.execute('SELECT * FROM udb')
-    for record in c.fetchall():
-        UDB[record[0]] = usr(record[0],
+async def read_db():
+    tmpdb = {}
+    db = await aiomysql.connect(host=g.sql_db_host, user=g.sql_db_usr,
+                                password=g.sql_db_pass, db='mysql')
+    c = await db.cursor()
+    await c.execute('SELECT * FROM grcbot.udb')
+    for record in await c.fetchall():
+        tmpdb[record[0]] = usr(record[0],
         address=record[1],
         last_faucet=record[2],
         balance=record[3],
         donations=record[4],
         lastTX=[record[5], record[6], record[7]])
     db.close()
+    return tmpdb
 
-def save_db(dbdir):
-    create_db(g.TMP_DIR)
-    db = sqlite3.connect(g.TMP_DIR)
-    c = db.cursor()
-    for u in UDB:
-        c.execute('INSERT INTO udb VALUES (?, ?, ?, ?, ?, ?, ?, ?)', (
-        UDB[u].usrID, UDB[u].address, UDB[u].last_faucet, UDB[u].balance, UDB[u].donations,
-        UDB[u].active_tx[0], UDB[u].active_tx[1], UDB[u].active_tx[2]))
-    db.commit()
+async def save_db(udb):
+    db = await aiomysql.connect(host=g.sql_db_host, user=g.sql_db_usr,
+                                password=g.sql_db_pass, db='mysql')
+    c = await db.cursor()
+    for u in udb:
+        await c.execute('SELECT * FROM grcbot.udb WHERE uid=%s', (u,))
+        if len(await c.fetchall()) > 0:
+            await c.execute('''UPDATE grcbot.udb SET
+                            last_faucet=%s,
+                            balance=%s,
+                            donations=%s,
+                            lastTX_amt=%s,
+                            lastTX_time=%s,
+                            lastTX_txid=%s;
+                            WHERE uid=%s''',
+                (udb[u].last_faucet, udb[u].balance, udb[u].donations,
+                udb[u].active_tx[0], udb[u].active_tx[1], udb[u].active_tx[2], udb[u].usrID))
+        else:
+            await c.execute('INSERT INTO grcbot.udb VALUES (%s, %s, %s, %s, %s, %s, %s, %s)', (
+            udb[u].usrID, udb[u].address, udb[u].last_faucet, udb[u].balance, udb[u].donations,
+            udb[u].active_tx[0], udb[u].active_tx[1], udb[u].active_tx[2]))
+    await db.commit()
     db.close()
-    sp.call('mv {} {}'.format(g.TMP_DIR, dbdir), shell=True)
