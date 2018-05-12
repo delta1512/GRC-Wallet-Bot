82578ae602676defcb6057e8a9393fd32c357e4f
Unknown <22047487+delta1512@users.noreply.github.com>
2018-05-12 19:21:17 +1000

    Upgraded to coinmarketcap api v2

--- GRC_pricebot.py
+++ GRC_pricebot.py
@@ -9,16 +9,16 @@ class price_bot:
     last_updated = 0
     last_price = 0
     timeout_sec = 380
-    price_url = 'https://api.coinmarketcap.com/v1/ticker/gridcoin/'
+    price_url = 'https://api.coinmarketcap.com/v2/ticker/833'
 
     async def price(self):
         if round(time()) > self.last_updated+self.timeout_sec:
             try:
                 async with aiohttp.ClientSession() as session:
                     async with session.get(self.price_url) as response:
-                        self.last_price = float(json.loads(await response.text())[0]['price_usd'])
+                        self.last_price = json.loads(await response.text())['data']['quotes']['USD']['price']
             except Exception as E:
-                logging.warning('[WARN] Error when trying to fetch USD price: '.format(E))
+                logging.warning('[WARN] Error when trying to fetch USD price: %s', E)
             self.last_updated = round(time())
         return self.last_price
 
