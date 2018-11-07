749b5dbfc3f5de7638eee2cd6e66036f96579fd1
Unknown <22047487+delta1512@users.noreply.github.com>
2018-11-08 10:30:12 +1100

    Switched to coingecko

--- grc_pricebot.py
+++ grc_pricebot.py
@@ -9,14 +9,14 @@ class price_bot:
     last_updated = 0
     last_price = 0
     timeout_sec = 380
-    price_url = 'https://api.coinmarketcap.com/v2/ticker/833'
+    price_url = 'https://api.coingecko.com/api/v3/coins/gridcoin-research'
 
     async def price(self):
         if round(time()) > self.last_updated+self.timeout_sec:
             try:
                 async with aiohttp.ClientSession() as session:
                     async with session.get(self.price_url) as response:
-                        self.last_price = json.loads(await response.text())['data']['quotes']['USD']['price']
+                        self.last_price = json.loads(await response.text())['market_data']['current_price']['usd']
             except Exception as E:
                 logging.warning('[WARN] Error when trying to fetch USD price: %s', E)
             self.last_updated = round(time())
